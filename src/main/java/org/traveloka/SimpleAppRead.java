81ade8b8f883c1373781c4f07f4aef0409b2a028
ariepratama <arie@traveloka.com>
2015-04-23 16:43:38 +0700

    add resource example

--- src/main/java/org/traveloka/SimpleAppRead.java
+++ src/main/java/org/traveloka/SimpleAppRead.java
@@ -66,25 +66,28 @@ public class SimpleAppRead {
     if (args.length < 1 || args[0] == null || args[0].isEmpty()){
       throw new NoTopicException("topic is empty");
     }
-    if (args.length < 2 || args[1] == null || args[1].isEmpty()){
-      throw new Exception("No Bucket Name");
-    }
-    if (args.length < 3 || args[2] == null || args[2].isEmpty()){
-      throw new Exception("no key provided");
-    }
-    if (args.length < 4 || args[3] == null || args[3].isEmpty()){
-      throw new Exception("no key id provided");
-    }
+//    if (args.length < 2 || args[1] == null || args[1].isEmpty()){
+//      throw new Exception("No Bucket Name");
+//    }
+//    if (args.length < 3 || args[2] == null || args[2].isEmpty()){
+//      throw new Exception("no key provided");
+//    }
+//    if (args.length < 4 || args[3] == null || args[3].isEmpty()){
+//      throw new Exception("no key id provided");
+//    }
 
     // handle s3 schema
-    AmazonS3 s3Client = new AmazonS3Client(new BasicAWSCredentials(args[3], args[2]));
-    S3Object s3Object = s3Client.getObject(new GetObjectRequest(args[1], args[2]));
-    AvroDecoder decoder = new AvroDecoder(s3Object.getObjectContent());
+//    AmazonS3 s3Client = new AmazonS3Client(new BasicAWSCredentials(args[3], args[2]));
+//    S3Object s3Object = s3Client.getObject(new GetObjectRequest(args[1], args[2]));
+    AvroDecoder decoder = new AvroDecoder(SimpleAppRead.class.getResource("schema/flight.visit.avsc").openStream());
     boolean readAsHadoopFile = false;
 
     // optional params
-    if (args.length >= 5 && (args[4] != null || ! args[4].isEmpty())){
-      readAsHadoopFile = Boolean.parseBoolean(args[4]);
+//    if (args.length >= 5 && (args[4] != null || ! args[4].isEmpty())){
+//      readAsHadoopFile = Boolean.parseBoolean(args[4]);
+//    }
+    if (args.length >= 2 && (args[1] != null || ! args[1].isEmpty())){
+      readAsHadoopFile = Boolean.parseBoolean(args[1]);
     }
     String topic = args[0];
 
