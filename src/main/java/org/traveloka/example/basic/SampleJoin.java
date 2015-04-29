b128773838b549bea568450cb038d658a72de4cc
ariepratama <arie@traveloka.com>
2015-04-29 11:08:24 +0700

    update sample...

--- src/main/java/org/traveloka/example/basic/SampleJoin.java
+++ src/main/java/org/traveloka/example/basic/SampleJoin.java
@@ -1,7 +1,54 @@
 package org.traveloka.example.basic;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.traveloka.helper.DebugUtility;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ariesutiono on 29/04/15.
  */
 public class SampleJoin {
+  private static final String[] KEYS = new String[]{"google", "bing", "direct"};
+
+
+  public static void main (String[] args){
+    SparkConf conf = new SparkConf().setAppName(SampleJoin.class.getSimpleName());
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    //construct rdd 1
+    List<Tuple2<String, Integer>> data1 = new ArrayList<Tuple2<String, Integer>>();
+    data1.add(new Tuple2<String, Integer>(KEYS[0], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[0], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[2], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[0], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[1], 100));
+
+    JavaPairRDD<String, Integer> rdd1 = sc.parallelizePairs(data1);
+    DebugUtility.logSomething("finished parallelized rdd1");
+
+    //construct rdd 2
+    List<Tuple2<String, Integer>> data2 = new ArrayList<Tuple2<String, Integer>>();
+    data1.add(new Tuple2<String, Integer>(KEYS[1], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[1], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[1], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[1], 100));
+    data1.add(new Tuple2<String, Integer>(KEYS[2], 100));
+
+    JavaPairRDD<String, Integer> rdd2 = sc.parallelizePairs(data2);
+    DebugUtility.logSomething("finished parallelized rdd2");
+
+    DebugUtility.logSomething("------------- BEGIN JOIN -------------");
+    JavaPairRDD<String, Tuple2<Integer, Integer>> rddJoin = rdd1.join(rdd2);
+    DebugUtility.printRdd(rddJoin, "JOIN");
+
+
+
+
+
+  }
 }
--- src/main/java/org/traveloka/helper/DebugUtility.java
+++ src/main/java/org/traveloka/helper/DebugUtility.java
@@ -1,7 +1,69 @@
 package org.traveloka.helper;
 
+import org.apache.log4j.Logger;
+import org.apache.spark.api.java.JavaPairRDD;
+import scala.Tuple2;
+
+import java.util.List;
+
 /**
  * Created by ariesutiono on 29/04/15.
  */
 public class DebugUtility {
+  private static final String tag = "[SPARK SAMPLE]";
+  private static final Logger logger = Logger.getLogger(tag);
+  private static final String stars = "***";
+
+  public static void logRdd(JavaPairRDD rdd, String tag){
+    List<Tuple2> dataset = rdd.collect();
+    logger.info("-------------------------------------------------------");
+    logger.info("Number of Retrieved dataset: " + dataset.size());
+    logger.info("-------------------------------------------------------");
+    for(Tuple2 datum: dataset) {
+      String msg = "[" + tag + "] key is= " + datum._1() + "value is= ";
+      if (byte[].class.toString().equals(datum._2().getClass().toString()))
+        logger.info(msg + new String((byte[]) datum._2()));
+      else {
+        Object obj = datum._2();
+        if (String.class.equals(obj.getClass())) {
+          logger.info(msg + datum._2());
+        }
+        else if(Tuple2.class.equals(obj.getClass())){
+          Tuple2 objTuple = (Tuple2) obj;
+          logger.info(msg + objTuple._1().toString() + objTuple._2().toString());
+        }
+      }
+    }
+
+  }
+
+  public static void logSomething(String msg){
+    logger.info(stars + msg + stars);
+  }
+
+  public static void printRdd(JavaPairRDD rdd, String tag){
+    List<Tuple2> dataset = rdd.collect();
+    System.out.println("-------------------------------------------------------");
+    System.out.println("Number of Retrieved dataset: " + dataset.size());
+    System.out.println("-------------------------------------------------------");
+    for(Tuple2 datum: dataset) {
+      String msg = "[" + tag + "] key is= " + datum._1() + "value is= ";
+      if (byte[].class.toString().equals(datum._2().getClass().toString()))
+        System.out.println(msg + new String((byte[]) datum._2()));
+      else {
+        Object obj = datum._2();
+        if (String.class.equals(obj.getClass())) {
+          System.out.println(msg + datum._2());
+        }
+        else if(Tuple2.class.equals(obj.getClass())){
+          Tuple2 objTuple = (Tuple2) obj;
+          System.out.println(msg + objTuple._1().toString() + objTuple._2().toString());
+        }
+      }
+    }
+  }
+
+  public static void printSomething(String msg){
+    System.out.println(tag + stars + msg + stars);
+  }
 }
